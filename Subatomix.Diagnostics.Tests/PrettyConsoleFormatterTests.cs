8c77a6b24868d89d4b7b159fbf76cdc6fcb2b554
Jeff Sharp <public@sharpjs.net>
2022-05-14 19:54:30 -0700

    More test coverage for PrettyConsoleFormatter.

--- Subatomix.Diagnostics.Tests/PrettyConsoleFormatterTests.cs
+++ Subatomix.Diagnostics.Tests/PrettyConsoleFormatterTests.cs
@@ -14,6 +14,9 @@
     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging.Console;
 using Subatomix.Testing;
 
 namespace Subatomix.Diagnostics;
@@ -46,15 +49,131 @@ public class PrettyConsoleFormatterTests
         h.Formatter.Options.Should().BeSameAs(optionsB);
     }
 
+    [Test]
+    public void IsColorEnabled_Default()
+    {
+        using var h = new TestHarness();
+
+        //h.Formatter.IsColorEnabled can be either true or false here
+
+        h.Formatter.IsConsoleRedirected = false;
+        h.Options.NotifyChanged();
+
+        h.Formatter.IsColorEnabled.Should().BeTrue();
+
+        h.Formatter.IsConsoleRedirected = true;
+        h.Options.NotifyChanged();
+
+        h.Formatter.IsColorEnabled.Should().BeFalse();
+    }
+
+    [Test]
+    public void IsColorEnabled_Enabled()
+    {
+        using var h = new TestHarness(options =>
+        {
+            options.ColorBehavior = LoggerColorBehavior.Enabled;
+        });
+
+        h.Formatter.IsColorEnabled.Should().BeTrue();
+
+        h.Formatter.IsConsoleRedirected = false;
+        h.Options.NotifyChanged();
+
+        h.Formatter.IsColorEnabled.Should().BeTrue();
+
+        h.Formatter.IsConsoleRedirected = true;
+        h.Options.NotifyChanged();
+
+        h.Formatter.IsColorEnabled.Should().BeTrue();
+    }
+
+    [Test]
+    public void IsColorEnabled_Disabled()
+    {
+        using var h = new TestHarness(options =>
+        {
+            options.ColorBehavior = LoggerColorBehavior.Disabled;
+        });
+
+        h.Formatter.IsColorEnabled.Should().BeFalse();
+
+        h.Formatter.IsConsoleRedirected = false;
+        h.Options.NotifyChanged();
+
+        h.Formatter.IsColorEnabled.Should().BeFalse();
+
+        h.Formatter.IsConsoleRedirected = true;
+        h.Options.NotifyChanged();
+
+        h.Formatter.IsColorEnabled.Should().BeFalse();
+    }
+
+    [Test]
+    [SetCulture("kl-GL")] // Kalaallisut (Greenland)
+    [TestCase(LogLevel.Trace,       "[??:??:??] ..... trace : message")]
+    [TestCase(LogLevel.Debug,       "[??:??:??] ..... debug : message")]
+    [TestCase(LogLevel.Information, "[??:??:??] ..... info  : message")]
+    [TestCase(LogLevel.Warning,     "[??:??:??] ..... warn  : message")]
+    [TestCase(LogLevel.Error,       "[??:??:??] .....  ERR  : message")]
+    [TestCase(LogLevel.Critical,    "[??:??:??] .....  RIP  : message")]
+    public void Write_Mono(LogLevel logLevel, string expected)
+    {
+        using var h = new TestHarness(options =>
+        {
+            options.ColorBehavior = LoggerColorBehavior.Disabled;
+        });
+
+        var entry = new LogEntry<Void>(
+            logLevel, "category", eventId: 42, state: default, exception: null,
+            (state, exception) => "message"
+        );
+
+        using var writer = new StringWriter();
+
+        h.Formatter.Write(entry, h.CreateScopeProvider(), writer);
+
+        writer.ToString().Should().Match(expected + Environment.NewLine);
+    }
+
+    [Test]
+    [SetCulture("kl-GL")] // Kalaallisut (Greenland)
+    [TestCase(LogLevel.Trace,       "~[90;38;5;239m[??:??:??] ~[34;38;5;23m..... ~[90;38;5;243mtrace : message~[0m")]
+    [TestCase(LogLevel.Debug,       "~[90;38;5;239m[??:??:??] ~[34;38;5;23m..... ~[90;38;5;243mdebug : message~[0m")]
+    [TestCase(LogLevel.Information, "~[37;38;5;242m[??:??:??] ~[36;38;5;31m..... ~[39minfo  : message~[0m")]
+    [TestCase(LogLevel.Warning,     "~[37;38;5;242m[??:??:??] ~[36;38;5;31m..... ~[33mwarn  : message~[0m")]
+    [TestCase(LogLevel.Error,       "~[37;38;5;242m[??:??:??] ~[36;38;5;31m..... ~[97;41;1m ERR ~[91;49m : message~[0m")]
+    [TestCase(LogLevel.Critical,    "~[37;38;5;242m[??:??:??] ~[36;38;5;31m..... ~[97;45;1m RIP ~[95;49m : message~[0m")]
+    public void Write_Color(LogLevel logLevel, string expected)
+    {
+        using var h = new TestHarness(options =>
+        {
+            options.ColorBehavior = LoggerColorBehavior.Enabled;
+        });
+
+        var entry = new LogEntry<Void>(
+            logLevel, "category", eventId: 42, state: default, exception: null,
+            (state, exception) => "message"
+        );
+
+        using var writer = new StringWriter();
+
+        h.Formatter.Write(entry, h.CreateScopeProvider(), writer);
+
+        writer.ToString().Replace('\x1B', '~').Should().Match(expected + Environment.NewLine);
+    }
+
     private class TestHarness : TestHarnessBase
     {
         public TestOptionsMonitor<Options> Options { get; }
 
         public PrettyConsoleFormatter Formatter { get; }
 
-        public TestHarness()
+        public TestHarness(Action<Options>? configure = null)
         {
-            Options   = new(Mocks);
+            Options = new TestOptionsMonitor<Options>(Mocks);
+            configure?.Invoke(Options.CurrentValue);
+
             Formatter = new PrettyConsoleFormatter(Options);
         }
 
@@ -62,6 +181,11 @@ public class PrettyConsoleFormatterTests
         {
             ((IDisposable) Formatter).Dispose();
             base.Verify();
+        }
+
+        public IExternalScopeProvider CreateScopeProvider()
+        {
+            return Mocks.Create<IExternalScopeProvider>().Object;
         }
     }
 }
