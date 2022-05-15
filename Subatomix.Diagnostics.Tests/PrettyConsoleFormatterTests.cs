751bfbd5af668f447f8b3050662c11bbd60a4434
Jeff Sharp <public@sharpjs.net>
2022-05-14 20:07:13 -0700

    More test coverage for PrettyConsoleFormatter.

--- Subatomix.Diagnostics.Tests/PrettyConsoleFormatterTests.cs
+++ Subatomix.Diagnostics.Tests/PrettyConsoleFormatterTests.cs
@@ -14,6 +14,7 @@
     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
+using System.Diagnostics;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Logging.Console;
@@ -111,6 +112,7 @@ public class PrettyConsoleFormatterTests
 
     [Test]
     [SetCulture("kl-GL")] // Kalaallisut (Greenland)
+    [TestCase(LogLevel.None,        "[??:??:??] .....       : message")]
     [TestCase(LogLevel.Trace,       "[??:??:??] ..... trace : message")]
     [TestCase(LogLevel.Debug,       "[??:??:??] ..... debug : message")]
     [TestCase(LogLevel.Information, "[??:??:??] ..... info  : message")]
@@ -138,6 +140,37 @@ public class PrettyConsoleFormatterTests
 
     [Test]
     [SetCulture("kl-GL")] // Kalaallisut (Greenland)
+    [TestCase(LogLevel.None,        "[??:??:??] #????       : message")]
+    [TestCase(LogLevel.Trace,       "[??:??:??] #???? trace : message")]
+    [TestCase(LogLevel.Debug,       "[??:??:??] #???? debug : message")]
+    [TestCase(LogLevel.Information, "[??:??:??] #???? info  : message")]
+    [TestCase(LogLevel.Warning,     "[??:??:??] #???? warn  : message")]
+    [TestCase(LogLevel.Error,       "[??:??:??] #????  ERR  : message")]
+    [TestCase(LogLevel.Critical,    "[??:??:??] #????  RIP  : message")]
+    public void Write_Mono_InActivity(LogLevel logLevel, string expected)
+    {
+        using var h = new TestHarness(options =>
+        {
+            options.ColorBehavior = LoggerColorBehavior.Disabled;
+        });
+
+        var entry = new LogEntry<Void>(
+            logLevel, "category", eventId: 42, state: default, exception: null,
+            (state, exception) => "message"
+        );
+
+        using var writer = new StringWriter();
+
+        using var activity = new Activity("Test").SetIdFormat(ActivityIdFormat.W3C).Start();
+
+        h.Formatter.Write(entry, h.CreateScopeProvider(), writer);
+
+        writer.ToString().Should().Match(expected + Environment.NewLine);
+    }
+
+    [Test]
+    [SetCulture("kl-GL")] // Kalaallisut (Greenland)
+    [TestCase(LogLevel.None,        "~[90;38;5;239m[??:??:??] ~[34;38;5;23m..... ~[90;38;5;243m      : message~[0m")]
     [TestCase(LogLevel.Trace,       "~[90;38;5;239m[??:??:??] ~[34;38;5;23m..... ~[90;38;5;243mtrace : message~[0m")]
     [TestCase(LogLevel.Debug,       "~[90;38;5;239m[??:??:??] ~[34;38;5;23m..... ~[90;38;5;243mdebug : message~[0m")]
     [TestCase(LogLevel.Information, "~[37;38;5;242m[??:??:??] ~[36;38;5;31m..... ~[39minfo  : message~[0m")]
@@ -157,6 +190,36 @@ public class PrettyConsoleFormatterTests
         );
 
         using var writer = new StringWriter();
+
+        h.Formatter.Write(entry, h.CreateScopeProvider(), writer);
+
+        writer.ToString().Replace('\x1B', '~').Should().Match(expected + Environment.NewLine);
+    }
+
+    [Test]
+    [SetCulture("kl-GL")] // Kalaallisut (Greenland)
+    [TestCase(LogLevel.None,        "~[90;38;5;239m[??:??:??] ~[34;38;5;23m#???? ~[90;38;5;243m      : message~[0m")]
+    [TestCase(LogLevel.Trace,       "~[90;38;5;239m[??:??:??] ~[34;38;5;23m#???? ~[90;38;5;243mtrace : message~[0m")]
+    [TestCase(LogLevel.Debug,       "~[90;38;5;239m[??:??:??] ~[34;38;5;23m#???? ~[90;38;5;243mdebug : message~[0m")]
+    [TestCase(LogLevel.Information, "~[37;38;5;242m[??:??:??] ~[36;38;5;31m#???? ~[39minfo  : message~[0m")]
+    [TestCase(LogLevel.Warning,     "~[37;38;5;242m[??:??:??] ~[36;38;5;31m#???? ~[33mwarn  : message~[0m")]
+    [TestCase(LogLevel.Error,       "~[37;38;5;242m[??:??:??] ~[36;38;5;31m#???? ~[97;41;1m ERR ~[91;49m : message~[0m")]
+    [TestCase(LogLevel.Critical,    "~[37;38;5;242m[??:??:??] ~[36;38;5;31m#???? ~[97;45;1m RIP ~[95;49m : message~[0m")]
+    public void Write_Color_InActivity(LogLevel logLevel, string expected)
+    {
+        using var h = new TestHarness(options =>
+        {
+            options.ColorBehavior = LoggerColorBehavior.Enabled;
+        });
+
+        var entry = new LogEntry<Void>(
+            logLevel, "category", eventId: 42, state: default, exception: null,
+            (state, exception) => "message"
+        );
+
+        using var writer = new StringWriter();
+
+        using var activity = new Activity("Test").SetIdFormat(ActivityIdFormat.W3C).Start();
 
         h.Formatter.Write(entry, h.CreateScopeProvider(), writer);
 
