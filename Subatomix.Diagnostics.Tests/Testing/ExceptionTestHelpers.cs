f56a1dbb30f42a7abcbc861c294affba8e9d94e7
Jeff Sharp <public@sharpjs.net>
2022-05-24 18:47:16 -0700

    Add overloads with default messages.

--- Subatomix.Diagnostics.Tests/Testing/ExceptionTestHelpers.cs
+++ Subatomix.Diagnostics.Tests/Testing/ExceptionTestHelpers.cs
@@ -21,6 +21,28 @@ namespace Subatomix.Diagnostics.Testing;
 /// </summary>
 internal static class ExceptionTestHelpers
 {
+    private const string
+        DefaultMessage = "A test exception was thrown.";
+
+    /// <summary>
+    ///   Creates an exception that has been thrown and caught.
+    /// </summary>
+    public static Exception Thrown()
+    {
+        return Thrown(DefaultMessage);
+    }
+
+    /// <summary>
+    ///   Creates an exception that has been thrown and caught.
+    /// </summary>
+    /// <param name="innerException">
+    ///   The exception that is the cause of the current exception.
+    /// </param>
+    public static Exception Thrown(Exception innerException)
+    {
+        return Thrown(DefaultMessage, innerException);
+    }
+
     /// <summary>
     ///   Creates an exception that has been thrown and caught.
     /// </summary>
